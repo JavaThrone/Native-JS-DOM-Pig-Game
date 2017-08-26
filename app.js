420466ff28f43d3b57f33d38dc01c1cf41a33f50
ApollonSkill <invaderzims@ukr.net>
2017-08-26 11:02:11 +0300

    it`s correct the end of game.

--- app.js
+++ app.js
@@ -13,16 +13,23 @@ document.addEventListener('DOMContentLoaded', function (event) {
     startPigGame();
 });
 
-var scores, roundScore, activePlayer, dice;
+var gamePlaying, scores, roundScore, activePlayer, dice;
 
 function startPigGame() {
-    onClickedStartNewGame();
+    initGame();
     document.getElementById('start-new-game').addEventListener("click", onClickedStartNewGame);
     document.getElementById('roll-dice').addEventListener("click", onClickedRoleDice);
     document.getElementById('hold').addEventListener("click", onClickedHold);
 }
 
 function onClickedStartNewGame() {
+    if (!gamePlaying) {
+        initGame();
+    }
+}
+
+function initGame() {
+    gamePlaying = true;
     scores = [0, 0];
     roundScore = 0;
     activePlayer = 0;
@@ -32,55 +39,59 @@ function onClickedStartNewGame() {
     document.getElementById('name-' + 0).classList.remove('winner');
     document.getElementById('name-' + 1).classList.remove('winner');
     document.getElementById('player-' + activePlayer + '-panel').classList.add('active');
-    document.getElementById('score-' + 0).innerHTML = scores[0];
-    document.getElementById('score-' + 1).innerHTML = scores[1];
-    document.getElementById('name-' + 0).innerHTML = 'PLAYER 1';
-    document.getElementById('name-' + 1).innerHTML = 'PLAYER 2';
-    document.getElementById('current-' + 0).innerHTML = roundScore;
-    document.getElementById('current-' + 1).innerHTML = roundScore;
+    document.getElementById('score-' + 0).textContent = scores[0];
+    document.getElementById('score-' + 1).textContent = scores[1];
+    document.getElementById('name-' + 0).textContent = 'PLAYER 1';
+    document.getElementById('name-' + 1).textContent = 'PLAYER 2';
+    document.getElementById('current-' + 0).textContent = roundScore;
+    document.getElementById('current-' + 1).textContent = roundScore;
     document.getElementById('img-dice').style.display = 'none';
-
 }
 
 function onClickedRoleDice() {
-    // var currentScoreElement = document.getElementById('current-' + activePlayer).innerHTML;
-    dice = Math.floor(Math.random() * 6) + 1;
-    if (dice !== 1) {
-        roundScore = roundScore + dice;
-        document.getElementById('current-' + activePlayer).innerHTML = roundScore;
-    } else {
-        changePlayer();
-    }
-    document.getElementById('img-dice').src = 'dice-' + dice + '.png';
+    // var currentScoreElement = document.getElementById('current-' + activePlayer).textContent;
+    if (gamePlaying) {
+        dice = Math.floor(Math.random() * 6) + 1;
+        if (dice !== 1) {
+            roundScore = roundScore + dice;
+            document.getElementById('current-' + activePlayer).textContent = roundScore;
+        } else {
+            changePlayer();
+        }
+        document.getElementById('img-dice').src = 'dice-' + dice + '.png';
 
-    if (document.getElementById('img-dice').style.display === 'none') {
-        document.getElementById('img-dice').style.display = 'block';
+        if (document.getElementById('img-dice').style.display === 'none') {
+            document.getElementById('img-dice').style.display = 'block';
+        }
     }
 }
 
 function onClickedHold() {
-    scores[activePlayer] = scores[activePlayer] + roundScore;
-    document.getElementById('score-' + activePlayer).innerHTML = scores[activePlayer];
-    if (scores[activePlayer] >= 100) {
-        document.getElementById('name-' + activePlayer).innerHTML = 'Winner';
-        document.getElementById('name-' + activePlayer).classList.add('winner');
-    } else {
-        changePlayer();
+    if (gamePlaying) {
+        scores[activePlayer] = scores[activePlayer] + roundScore;
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+        if (scores[activePlayer] >= 20) {
+            gamePlaying = false;
+            document.getElementById('name-' + activePlayer).textContent = 'Winner';
+            document.getElementById('name-' + activePlayer).classList.add('winner');
+            document.querySelector('.dice').style.display = 'none';
+            document.getElementById('player-' + activePlayer + '-panel').classList.remove('active');
+        } else {
+            changePlayer();
+        }
     }
-
-
 }
 
 function changePlayer() {
     roundScore = 0;
-    document.getElementById('current-' + activePlayer).innerHTML = roundScore;
+    document.getElementById('current-' + activePlayer).textContent = roundScore;
     // player-0-panel active
-    document.getElementById('player-' + activePlayer + '-panel').classList.remove('active');
     if (activePlayer === 0) {
         activePlayer = 1;
     } else {
         activePlayer = 0;
     }
-    document.getElementById('player-' + activePlayer + '-panel').classList.add('active');
+    document.getElementById('player-0-panel').classList.toggle('active');
+    document.getElementById('player-1-panel').classList.toggle('active');
 }
 
